f02fecb43751a469a7bb664e56e95cd6324057ea
yanziwu999 <767769407@qq.com>
2021-05-30 17:20:55 +0800

    ${emoji[$(($RANDOM Y-d M:S)

---
+++ 自动提交.bat
@@ -0,0 +1,11 @@
+emoji=( ♈️ ♉ ♊️  ♋️  ♌️ 	♍️ 	♎️  ♏️ 	♐️  ♑️ ♒️ 	♓️ 	⛎  🔯 )
+git add -A
+echo "Add Finish"
+git commit -am "${emoji[$(($RANDOM % ${#emoji[@]} + 1 ))]} cytest  push at $(date "+%Y-%m-%d %H:%M:%S")"
+echo "Commit Finish"
+git pull
+echo "Pull Finish"
+git status
+echo "Status Finish"
+git push
+echo "push Finish"
